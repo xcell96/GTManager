4b1c84b2bfaa95d1e85bb80883c971fd33f3b47f
Deliu Marius <marius.profesional@gmail.com>
2025-10-27 10:47:04 +0200

    Updated project to be of type Spring Boot

---
+++ src/main/java/com/xcell/PageMe/PageMeApplication.java
@@ -0,0 +1,13 @@
+package com.xcell.PageMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageMeApplication.class, args);
+	}
+
+}
