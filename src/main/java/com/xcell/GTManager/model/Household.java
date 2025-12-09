034f9c4f8969229b87cff4d2e51bf3ac76f1cc2d
Deliu Marius <marius.profesional@gmail.com>
2025-12-10 00:43:22 +0200

    Inceperea modelelor de date

---
+++ src/main/java/com/xcell/GTManager/model/Household.java
@@ -0,0 +1,21 @@
+package com.xcell.GTManager.model;
+
+/*
+ * household_id
+ * address
+ * surface
+ *
+ * cattle
+ * swine
+ * sheep
+ * goats
+ * equines
+ * poultry
+ * rabbits
+ * donkeys
+ * bee_families
+ * other_animals
+ */
+
+public class Household {
+}
---
+++ src/main/java/com/xcell/GTManager/model/Person.java
@@ -0,0 +1,23 @@
+package com.xcell.GTManager.model;
+
+/*
+ * person_id
+ * first_name
+ * last_name
+ * sex
+ * DOB
+ * CNP
+ * citizenship
+ *
+ * household_id (FK)
+ * is_head (T/F)
+ * kinship
+ *
+ * education level
+ * degree
+ * job
+ * place_of_work
+ */
+
+public class Person {
+}
